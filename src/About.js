676ac7ad2d0d759c9132e045fb0d74877ad8838d
DaJon Ewing <dajone37@gmail.com>
2023-03-27 19:23:28 -0700

    fix to about us

--- src/About.js
+++ src/About.js
@@ -4,7 +4,15 @@ class Profile extends Component {
 
   render() {
     /* TODO: render information about the developers */
-    return <p>After serving in the US Navy for 8 years, as a leading supervisor you tend to get many experiences. Having over 90 sailors to provide guidance and technical expertise support. To now taking technical support to another level, following a long-lost dream of computers and their make-up. The under-lining language behind the finished product of any technology has always been something that really intrigued my interest. Being a Dev has allowed me to move into the direction of understanding and not only has it dressed an open wound, but it has actually showed me that there really isn't a wound at all. Every Role that I am blessed to fulfil I do so at the highest of standards and leave absolutely no doubts of prosperity. </p>
+    return (
+    <>
+    <h1>About Us</h1>
+    <h2>Alex Carr</h2>
+    <p>After serving in the US Navy for 8 years, as a leading supervisor you tend to get many experiences. Having over 90 sailors to provide guidance and technical expertise support. To now taking technical support to another level, following a long-lost dream of computers and their make-up. The under-lining language behind the finished product of any technology has always been something that really intrigued my interest. Being a Dev has allowed me to move into the direction of understanding and not only has it dressed an open wound, but it has actually showed me that there really isn't a wound at all. Every Role that I am blessed to fulfil I do so at the highest of standards and leave absolutely no doubts of prosperity.</p> 
+    <h2>DaJon Ewing</h2>
+   <p>I am a software developer and US Navy veteran residing in Port Orchard, Wa with a background in Logistical Operations and Inventory Management. I am dedicated to making a difference in the constantly evolving tech industry through software development.</p>
+   </>
+    )
   }
 };
 
